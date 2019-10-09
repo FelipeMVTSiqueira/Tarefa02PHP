61892c7ce27b4b8b8a4f6e478fbbc3acfc432634
unknown <felipesiqueira1212@gmail.com>
2019-10-09 11:08:02 -0300

    exercicio 2 da tarefa 2

--- tarefa02.php
+++ tarefa02.php
@@ -4,6 +4,14 @@
     if($num1>$num2){
         echo "O maior número é: ".$num1;
     } else {
-        echo "O maior número é: ".$num2;
+        echo "O maior número é: ".$num2."<br>"."<br>";
     }
+?>
+
+<?php
+    $tresOuCinco=mt_rand(1,5);
+    if($tresOuCinco == 3 || $tresOuCinco == 5){
+        echo "o numero aleatorio é: ".$tresOuCinco;
+    } else "o nomero aleatorio é diferente de 3 ou 5";
+
 ?>
