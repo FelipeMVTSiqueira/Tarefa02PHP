d184df817ebb31e576e09fadb6ae1d43a632c822
unknown <felipesiqueira1212@gmail.com>
2019-10-09 11:24:33 -0300

    inicio do exercicio 5 tarefa02

--- tarefa02.php
+++ tarefa02.php
@@ -11,9 +11,16 @@
 <?php
     $tresOuCinco=mt_rand(1,5);
     if($tresOuCinco == 3 || $tresOuCinco == 5){
-        echo "o numero aleatorio é: ".$tresOuCinco;
+        echo "o numero aleatorio é: ".$tresOuCinco."<br>"."<br>";
     } else if($tresOuCinco != 3){
-        echo "o numero não é 3 porra!";
-    } else echo $tresOuCinco;
+        echo "o numero não é 3 porra!"."<br>"."<br>";
+    } else echo $tresOuCinco."<br>"."<br>";
 
+?>
+
+<?php 
+    $umACem=mt_rand(1,100);
+    if($umACem>50){
+        echo "o numero aleatorio é maior que 50"."<br>"."<br>";
+    } else echo "o numero aleatorio é menor que 50"."<br>"."<br>";
 ?>
